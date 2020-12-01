d5f34239898154edcc00e8eb037b55ddf085d668
Sandy <perezsandy775@gmail.com>
2020-12-01 19:51:37 -0300

    products db

--- LSCRsupplies/src/data/lscrdb_Products.sql
+++ LSCRsupplies/src/data/lscrdb_Products.sql
@@ -1,10 +1,4 @@
 SELECT * from products; 
-INSERT INTO products VALUES (11,'Cuaderno A4 rayado tapa blanda Universo', 'Tu compañero perfecto, porque es práctico y liviano. Divertido para organizarte, crear y escribir en el instituto, la facu, ¡o donde querás! 
-Incluye:
-Planner anual
-Planner mensual
-Horarios
-Índice
-Regla
-Cierre elástico', '' , 11, 1,849);
-SELECT * FROM products
+INSERT INTO products VALUES (20,'Cuaderno 21x27 ABC rayado espiralado x100 hojas', 'Cuaderno ABC
+21x27 rayado
+espiralado x100 hojas.', '' ,12,1,450);
