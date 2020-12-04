6f8f7633d6cecb24d99af848ab46b4d91fd390a2
cynlofer <69534780+cynlofer@users.noreply.github.com>
2020-12-03 23:17:41 -0300

    cambios CSS

--- LSCRsupplies/src/data/lscrDB_1.sql
+++
@@ -1,9 +0,0 @@
-SELECT * FROM products;
-INSERT INTO products VALUE (DEFAULT,'Set 24 Acuarelas Cotman Winsor & Newton','Set 24 Acuarelas Cotman Winsor & Newton - Contiene 24 pastillas de acuarela Cotman
-- Una paleta integrada en su estuche
-- Una paleta desmontable','','9','2','8340');
-SELECT * FROM products;
-INSERT INTO products VALUES (DEFAULT, 'Bastidor Seurat 50x60', 'Bastidor Seurat 50x60 CM Tela estudio grano fino','','6','2','660');
-INSERT INTO products VALUES (DEFAULT,'Lata 12 Lapices Staedtler Tradition De Graduación','Los Staedtler Tradition son lapices de alta calidad para escribir, dibujar y esbozar.Increíble resistencia a la rotura gracias a la especial formulación de la mina y a su doble encoladura.Madera certificada, procedente de bosques de gestión sostenible.Lata con 12 graduaciones: 6B, 5B, 4B, 3B, 2B, B, HB, F, H, 2H, 3H y 4H.','','7','2','1270');
-INSERT INTO products VALUES (DEFAULT,'Lata 12 Lapices Staedtler Tradition','Son lapices de alta calidad para escribir, dibujar y esbozar.Increíble resistencia a la rotura.Madera procedente de bosques de gestión sostenible.graduaciones:6B,5B,4B,3B,2B,B,HB,F,H,2H,3H y 4H.','','7','2','1270');
-SELECT * FROM products;
